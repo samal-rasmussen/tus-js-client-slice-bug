7c123183cb5f098710ff0ce8c2f8be73554b91ff
Sámal Rasmussen <samal84@gmail.com>
2022-03-11 12:18:59 +0000

    Implement reproduction

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Button, View } from 'react-native';
+import { Upload } from 'tus-js-client'
+
+const host = ''
+
+const mov = require('./assets/movie.mov')
+const uri = Image.resolveAssetSource(mov).uri
+
+async function upload() {
+  var upload = new Upload({uri} as any, {
+    endpoint: "http://" + host + ":1080/files/",
+    chunkSize: 1024*1024,
+    onError: function(error) {
+        console.log("Failed because: " + error)
+    },
+    onProgress: function(bytesUploaded, bytesTotal) {
+        var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
+        console.log(bytesUploaded, bytesTotal, percentage + "%")
+    },
+    onSuccess: function() {
+        console.log("Download %s from %s", upload.file, upload.url)
+    }
+  })
+  upload.start()
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="upload" onPress={upload} />
     </View>
   );
 }
---
+++ readme.md
@@ -0,0 +1,14 @@
+This repo contains an app and a server implementation.
+
+Requires Expo cli to be installed: `npm install -g expo-cli`
+Also required the Expo Go app on your ios device: `https://expo.dev/client`
+
+# App
+
+* Run `npm i`
+* Put in ip of the computer that you run the server on into the host variable in App.tsx. Most likely your own pcs ip.
+* Run `expo start`
+
+# Server
+
+* Run `node server.js`
